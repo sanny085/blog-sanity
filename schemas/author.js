6924eca5e11c4c1f447bcf136bc95f699b2e2240
sanny085 <sannykumar085.com>
2021-09-21 14:23:43 +0530

    upload folder

---
+++ schemas/author.js
@@ -0,0 +1,54 @@
+export default {
+  name: 'author',
+  title: 'AuthorName',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      description : 'first and last name',
+      type: 'string',
+    }, 
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 96,
+      },
+    },
+    {
+      name: 'date',
+      title: 'DOB',
+      type: 'date',
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'bio',
+      title: 'Bio',
+      type: 'array',
+      of: [
+        {
+          title: 'Block',
+          type: 'block',
+          styles: [{title: 'Normal', value: 'normal'}],
+          lists: [],
+        },
+      ],
+    },
+  ],
+  preview: {
+    select: {
+      title: 'name',
+      media: 'image',
+    },
+  },
+}
